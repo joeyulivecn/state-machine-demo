acc6f3151c97c3b122e69a43e77c912f73aa96e1
Joe Yu <joeyu@live.cn>
2019-03-31 22:09:52 +0800

    first commit

---
+++ src/main/java/com/joe/statemachinedemo/order/OrderStatusChangeEvent.java
@@ -0,0 +1,10 @@
+package com.joe.statemachinedemo.order;
+
+/**
+ * Created by lijingyao on 2017/11/24 11:43.
+ */
+public enum OrderStatusChangeEvent {
+
+    // 支付，发货，确认收货,用户退货（款）
+    PAYED, DELIVERY, RECEIVED,REFUND
+}
